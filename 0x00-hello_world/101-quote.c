7f1a08b987e4163183536ab2f22bddc24a95727e
Mack Octavian <maestrooctavian@gmail.com>
2023-03-16 15:48:57 -0700

    Updated linting to 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -7,6 +6,7 @@
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, "
+"2015-10-19\n");
 return (1);
 }
