b2cf66598b08611b4dcaf36875b738c0c32fc973
Mack Octavian <maestrooctavian@gmail.com>
2023-03-16 14:50:00 -0700

    Fixed some linting errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,6 +7,5 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
