84bac119dc001b22d63be05f482f9aa8f20cf54d
Mack Octavian <maestrooctavian@gmail.com>
2023-03-30 13:24:23 -0700

    Fixed Betty error due to line exceeding 80 characters

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,9 @@
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  *
- * Return: If str1 < str2, the negative difference of the first unmatched characters.
+ * Return: If str1 < str2, the negative differ of the first unmatched chars.
  *         If str1 == str2, 0.
- *         If str1 > str2, the positive difference of the first unmatched characters.
+ *         If str1 > str2, the positive differ of the first unmatched chars.
  */
 int _strcmp(char *s1, char *s2)
 {
