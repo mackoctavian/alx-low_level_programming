fbdaa83136ff776e002cbba3d61d81434c8d4014
Mack Octavian <maestrooctavian@gmail.com>
2023-04-10 16:16:26 -0700

    Added various .c file

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: Mack Octavian
- * Desc: Header file containing the prototypes of all functions
- *       included in the static library libmy.a.
- */
 
 int _putchar(char c);
 int _islower(int c);
@@ -28,5 +22,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 
 #endif
