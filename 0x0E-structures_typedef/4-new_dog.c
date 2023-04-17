61c7970c59647fd91978324a90681207e3d5795b
Mack Octavian <maestrooctavian@gmail.com>
2023-04-17 13:59:50 -0700

    Fixed error in dog.h file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ int _strlen(char *str)
 }
 
 /**
- * _strcopy - Copies a string pointed to by src, including the
+ * _strcopy - Copies a string pointed to, including the
  *            terminating null byte, to a buffer pointed to by dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
