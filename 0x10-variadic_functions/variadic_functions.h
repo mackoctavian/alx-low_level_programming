ef4a1b3cc26b0ce5f129efd1875d605dbec8d004
Mack Octavian <maestrooctavian@gmail.com>
2023-04-20 10:45:18 -0700

    Added header file for the project

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+/**
+  *variadic_func.h: Header file containing prototypes for all function used.
+  *
+  */
+
+int sum_them_all(const unsigned int n, ...);
+#endif
