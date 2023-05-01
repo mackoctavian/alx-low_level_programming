fdb9209b51c16793478741048814341c06aea467
Mack Octavian <maestrooctavian@gmail.com>
2023-05-01 02:11:59 -0700

    Added a function that prints all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+/**
+  * Author: Mack Octavian
+  * File: 0-print_listint.c
+  * Description: a function that prints all the elements
+  *	of a listint_t list
+  */
+#include "lists.h"
+
+/**
+  * print_listint - a function that prints all the elements
+  *		    of a listint_t listof.
+  * @h: A Pointer to the listint_t struct.
+  *
+  * Return: the number of nodes.
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	for (; h != NULL; h = h->next;)
+		count++;
+	return (count);
+}
